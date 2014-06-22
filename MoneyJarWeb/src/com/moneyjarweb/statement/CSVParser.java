5f1beeb351ce01477ea67eaeda4473060ef578bd
Kho Minh Vi <khominh.vi@gmail.com>
2014-06-22 16:23:59 +0100

    Edited log information to be more readable and informative.

--- MoneyJarWeb/src/com/moneyjarweb/statement/CSVParser.java
+++ MoneyJarWeb/src/com/moneyjarweb/statement/CSVParser.java
@@ -17,19 +17,63 @@ public class CSVParser implements StatementParser {
 
 	private CSVReader csvReader;
 	private Logger logger;
-	
-	public CSVParser () {
+
+	public CSVParser() {
 		logger = Logger.getLogger(CSVParser.class);
 	}
 
 	@Override
 	public List<Transaction> parseStatement(File statement) throws Exception {
 
-		// Read CSV file.
-		ArrayList<String[]> csvData = readCSV(statement);
+		logger.info(">> parseStatement() - parsing file: "
+				+ statement.getName());
 
-		// Convert CSV data to transaction objects
-		List<Transaction> transactions = parseCSVData(csvData);
+		ArrayList<String[]> csvContent = readCSVFile(statement);
+		List<Transaction> transactions = parseCSVData(csvContent);
+
+		logger.info("<< parseStatement() - returning " + transactions.size()
+				+ " transactions");
+
+		return transactions;
+	}
+
+	private ArrayList<String[]> readCSVFile(File statement) throws Exception {
+
+		logger.debug(">> readCSVFile() - reading file: " + statement.getName());
+
+		ArrayList<String[]> transactions = new ArrayList<String[]>();
+		String[] nextLine;
+
+		try {
+			csvReader = new CSVReader(new FileReader(statement));
+			while ((nextLine = csvReader.readNext()) != null) {
+				transactions.add(nextLine);
+			}
+		} catch (FileNotFoundException e) {
+			String errorMsg = "The file \""
+					+ statement.getPath()
+					+ "\" could not be found, please check that the file exists.";
+			logger.warn(errorMsg);
+			throw new Exception(errorMsg, e);
+
+		} catch (IOException e) {
+			String errorMsg = "Problem reading the file. The file may be corrupted.";
+			logger.error(errorMsg, e);
+			throw new Exception(errorMsg, e);
+
+		} finally {
+			try {
+				if (csvReader != null)
+					csvReader.close();
+			} catch (IOException e) {
+				String errorMsg = "Problem closing the file, the resource may not be available.";
+				logger.error(errorMsg, e);
+				throw new Exception(errorMsg, e);
+			}
+		}
+
+		logger.debug("<< readCSVFile() - successfully read "
+				+ transactions.size() + " entries");
 
 		return transactions;
 	}
@@ -37,58 +81,27 @@ public class CSVParser implements StatementParser {
 	private List<Transaction> parseCSVData(ArrayList<String[]> csvData)
 			throws Exception {
 
-		ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+		logger.debug(">> parseCSVData() - parsing " + csvData.size()
+				+ " entries");
 
-		for (String[] column : csvData) {
+		ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
+		for (String[] entry : csvData) {
 			Transaction transaction = new Transaction();
 			try {
-				// TODO Replace numbers with constants?
-				transaction.setDate(column[0]);
-				transaction.setDescription(column[1]);
-				transaction.setAmount(column[2]);
-				transactions.add(transaction);
+				// TODO Replace magic numbers with constants/property values.
+				transaction.setDate(entry[0]);
+				transaction.setDescription(entry[1]);
+				transaction.setAmount(entry[2]);
+				transactionList.add(transaction);
 			} catch (ArrayIndexOutOfBoundsException e) {
-				throw new Exception(
-							"Could not parse CSV data, " + 
-							"file format may not be supported.", e);
+				throw new Exception("Could not parse CSV data, "
+						+ "file format may not be supported.", e);
 			}
 		}
-		
-		return transactions;
-	}
 
-	private ArrayList<String[]> readCSV(File statement) throws Exception {
-		
-		ArrayList<String[]> transactions = new ArrayList<String[]>();
-		String[] nextLine;
-		
-		try {
-			csvReader = new CSVReader(new FileReader(statement));
-			while ((nextLine = csvReader.readNext()) != null) {
-				transactions.add(nextLine);
-			}
-		} catch (FileNotFoundException e) {
-			String errorMsg = "The file \"" + statement.getPath() + 
-					"\" could not be found, please check that the file exists.";
-			logger.warn(errorMsg);
-			throw new Exception(errorMsg, e);
-			
-		} catch (IOException e) {
-			String errorMsg = "Problem reading the file. The file may be corrupted.";
-			logger.error(errorMsg, e);
-			throw new Exception(errorMsg, e);
-			
-		} finally {
-			try {
-				if (csvReader != null)
-					csvReader.close();
-			} catch (IOException e) {
-				String errorMsg = "Problem closing the file, the file may be corrupted.";
-				logger.error(errorMsg, e);
-				throw new Exception(errorMsg, e);
-			}
-		}
-		return transactions;
-	}
+		logger.debug("<< parseCSVData() - successfully parsed "
+				+ transactionList.size() + " transactions");
 
+		return transactionList;
+	}
 }

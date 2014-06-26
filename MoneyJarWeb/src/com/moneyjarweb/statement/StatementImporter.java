5a438e9226a05733873bae876add537b47d90681
Kho Minh Vi <khominh.vi@gmail.com>
2014-06-26 17:21:30 +0100

    Added call to persistence library to store parsed objects to database. Then refactored to make code more readable.

--- MoneyJarWeb/src/com/moneyjarweb/statement/StatementImporter.java
+++ MoneyJarWeb/src/com/moneyjarweb/statement/StatementImporter.java
@@ -3,42 +3,64 @@ package com.moneyjarweb.statement;
 import java.io.File;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.moneyjarweb.transaction.Transaction;
 
 public class StatementImporter {
-	
-	private File statement;
 
-	public StatementImporter() {}
-	
+	private Logger logger = Logger.getLogger(StatementImporter.class);
+
 	public void statementImport(File statement) throws Exception {
-		
-		this.statement = statement;
-		
-		// 1. Determine the type of file
-		String fileExtension = fileType(statement);
+		logger.debug(">> statementImport()");
+		logger.info("-- statementImport() -- Importing file:"
+				+ statement.getName());
 
-		// 2. Call correct parser
-		StatementParser parser;
+		String fileExtension = getFileExtension(statement);
+		StatementParser parser = getParser(fileExtension);
+
+		List<Transaction> transactions = null;
+		if (parser != null) {
+			transactions = parser.parseStatement(statement);
+		}
+
+		if (transactions != null) {
+			TransactionDAO tdao = new TransactionDAO();
+			tdao.create(transactions);
+		}
+	}
+
+	public String getFileExtension(File file) {
+
+		logger.debug(">> fileType() - retrieving file type");
+
+		String name = file.getName();
+		int dot = name.lastIndexOf('.');
+		String extension = name.substring(dot + 1);
+
+		logger.debug("<< fileType() - returning '" + extension
+				+ "' as file extension");
+
+		return extension;
+	}
+
+	public StatementParser getParser(String fileExtension) {
+
+		logger.debug(">> getParser() -- retrieving correct parser for extension:"
+				+ fileExtension);
+
+		// Can be replaced with factory pattern as new parsers are added to
+		// project
+		StatementParser parser = null;
 		if (fileExtension.equals("csv")) {
-			 parser = new CSVParser();
+			parser = new CSVParser();
 		} else {
 			// Default parser
 			parser = new CSVParser();
 		}
-		
-		// 3. Parse file to transactions
-		List<Transaction> transactions = parser.parseStatement(statement);
-	
-		// 4. Commit transactions to database
-		
-	}
-	
-	public String fileType(File file){
-		String name = file.getName(); 		
-		int dot = name.lastIndexOf('.');
 
-		return name.substring(dot + 1);
+		logger.debug("<< getParser() -- returning parser: " + parser.getClass());
+		return parser;
 	}
-	
+
 }

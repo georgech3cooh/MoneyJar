76f976efa86eb7b3e810b7752e8656c10bc6d191
Kho Minh Vi <khominh.vi@gmail.com>
2014-10-28 20:35:08 +0000

    Removed the name parameter from the request. Name is now retrieved from MultipartFile original name method.

--- MoneyJar/src/main/java/com/moneyjar/FileUploadController.java
+++ MoneyJar/src/main/java/com/moneyjar/FileUploadController.java
@@ -4,6 +4,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import jxl.common.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,32 +16,42 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 
+	Logger logger = Logger.getLogger(FileUploadController.class);
+	
+	@RequestMapping(value = "/upload", method = RequestMethod.GET) 
+	public String pageRequest() {
+		return "upload";
+	}
+	
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public String handleFileUpload(@RequestParam("name") String name,
-			@RequestParam("file") MultipartFile file, Model model) {
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, 
+									Model model) {
+		String name = file.getOriginalFilename();
+		logger.debug("Uploading " + name + " to server.");
 		if (!file.isEmpty()) {
 			try {
 				byte[] bytes = file.getBytes();
 				BufferedOutputStream stream = new BufferedOutputStream(
-						new FileOutputStream(new File(name + "-uploaded")));
+						new FileOutputStream(new File(name)));
 				stream.write(bytes);
 				stream.close();
-
 				model.addAttribute("message", "The file " + name
-						+ "was successfully uploaded.");
+						+ " was successfully uploaded.");
+				logger.debug("File upload successful.");
 				return "success";
 
 			} catch (Exception e) {
 				model.addAttribute("message", "The file" + name
 						+ " could not be uploaded - because " + e.getMessage());
+				logger.debug("File upload failed.");
 				return "failure";
 			}
 		} else {
 			model.addAttribute("message", "The file " + name
 					+ "could not be uploaded, because the file is empty.");
+			logger.debug("File upload failed.");
 			return "failure";
 		}
-
 	}
 
 }

310f52db94a72bb781fdb2411b1b99d1e2c10d4a
Kho Minh Vi <khominh.vi@gmail.com>
2014-10-29 15:08:56 +0000

    Formatted document to make it more readable. And corrected variable declaration of uploadedFile.

--- MoneyJar/src/main/java/com/moneyjar/FileUploadController.java
+++ MoneyJar/src/main/java/com/moneyjar/FileUploadController.java
@@ -17,60 +17,66 @@ import com.moneyjar.statement.StatementImporter;
 
 @Controller
 public class FileUploadController {
-	
+
 	@Autowired
 	StatementImporter statementImporter;
 
 	String systemUploadPath = "/opt/upload/";
 	Logger logger = Logger.getLogger(FileUploadController.class);
-	
-	@RequestMapping(value = "/upload", method = RequestMethod.GET) 
+
+	@RequestMapping(value = "/upload", method = RequestMethod.GET)
 	public String pageRequest() {
 		return "upload";
 	}
-	
+
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, 
-									Model model) {
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			Model model) {
+		
 		String fileName = file.getOriginalFilename();
 		logger.debug("Uploading " + fileName + " to server.");
+		
 		if (!file.isEmpty()) {
+			File uploadedFile = new File(systemUploadPath, fileName);
+			
 			try {
 				byte[] bytes = file.getBytes();
-				
-				File uploadedFile = new File(systemUploadPath, fileName);
-				
+
 				BufferedOutputStream stream = new BufferedOutputStream(
 						new FileOutputStream(uploadedFile));
 				stream.write(bytes);
 				stream.close();
-				logger.debug("File stored to local file system " 
-								+ uploadedFile.getAbsolutePath());
-				
+				logger.debug("File stored to local file system "
+						+ uploadedFile.getAbsolutePath());
+
 				statementImporter.importStatement(uploadedFile);
 				logger.debug("File upload and persistence completed.");
-				
+
 				model.addAttribute("message", "The file " + fileName
 						+ " was successfully uploaded.");
-				
+
 				return "success";
 
 			} catch (Exception e) {
 				model.addAttribute("message", "The file" + fileName
 						+ " could not be uploaded - because " + e.getMessage());
+				
 				logger.info("File upload failed - because" + e.getMessage());
+				
 				return "failure";
+				
 			} finally {
 				logger.debug("Deleting uploaded file.");
+				
 				Boolean deleted = uploadedFile.delete();
-				if (!deleted) {
-					logger.error("The working file was not deleted.");
-				}
+				if (!deleted) logger.error("The working file was not deleted.");
 			}
 		} else {
 			model.addAttribute("message", "The file " + fileName
 					+ "could not be uploaded, because the file is empty.");
+			
 			logger.info("File upload failed.");
+			
 			return "failure";
 		}
 	}

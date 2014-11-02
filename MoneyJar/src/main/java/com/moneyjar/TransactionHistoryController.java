e59c5ae29b2fc2bf0a5d9a5c87812aa0228d5568
Kho Minh Vi <khominh.vi@gmail.com>
2014-11-02 23:30:40 +0000

    Added content negotiation, and handler for JSON requests.

--- MoneyJar/src/main/java/com/moneyjar/TransactionHistoryController.java
+++ MoneyJar/src/main/java/com/moneyjar/TransactionHistoryController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.moneyjar.statement.TransactionDao;
 import com.moneyjar.transaction.Transaction;
@@ -22,6 +23,7 @@ public class TransactionHistoryController {
 	@Autowired
 	TransactionDao transactionDao;
 	static Logger logger = Logger.getLogger(TransactionHistoryController.class);
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
 
 	@RequestMapping(value = "/transactions", method = RequestMethod.GET)
 	public String getTransactionHistoryRange(
@@ -32,13 +34,7 @@ public class TransactionHistoryController {
 		logger.debug("Transaction history request called: from [" + from
 				+ "] to [" + to + "]");
 
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		Date fromDate = sdf.parse(from);
-		Date toDate = sdf.parse(to);
-
-		List<Transaction> transactions = transactionDao.getDateRange(fromDate,
-				toDate);
-		logger.debug("Retrieved transactions " + transactions.size());
+		List<Transaction> transactions = retrieveTransactions(from, to);
 
 		model.addAttribute("from", from);
 		model.addAttribute("to", to);
@@ -46,4 +42,25 @@ public class TransactionHistoryController {
 		return "transaction-history";
 	}
 
+	@RequestMapping(value="/transactions", produces={"application/json"})
+	public @ResponseBody List<Transaction> getTransactionHistoryRange(@RequestParam("from-date") String from,
+			@RequestParam("to-date") String to) throws ParseException{
+		logger.debug("JSON transaction history request called.");
+		
+		List<Transaction> transactions = retrieveTransactions(from, to);
+		
+		return transactions;
+	}
+	
+	// Utility Method - Used to retrieve transactions from database
+	private List<Transaction> retrieveTransactions(String from, String to) throws ParseException {
+		List<Transaction> transactions = null;
+		Date fromDate = sdf.parse(from);
+		Date toDate = sdf.parse(to);
+		
+		transactions = transactionDao.getDateRange(fromDate, toDate);
+		logger.debug("Retrieved transaction " + transactions.size());
+		
+		return transactions;
+	}
 }
